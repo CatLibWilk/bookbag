259a2e2a31bc14358737fb98c6b99245361e3129
Andy Wilk <andrewdwilk@gmail.com>
2018-10-07 14:06:00 -0400

    jumbo and navbar build

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Jumbotron = ({ children }) => (
-  <div
-    style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-    className="jumbotron"
-  >
-    {children}
+  <div class="jumbotron jumbotron-fluid mb-0">
+    <div class="container">
+      <h1 class="display-4">BookBag Ultra+</h1>
+      <p class="lead">Your notebook, in your browser.</p>
+    </div>
   </div>
 );
 
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import API from "../../utils/API";
 
+import Jumbotron from "../../components/Jumbotron";
+import Navbar from "../../components/Navbar";
+
 class HomePage extends Component {
   render() {
     return(
-      <h1>HomePage</h1>
+      <div>
+        <Jumbotron />
+        <Navbar />
+      </div>
       )
   }
 }
