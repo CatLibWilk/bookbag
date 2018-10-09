b4d336752f87914f65f581e06b0ae023b81fef55
Andy Wilk <andrewdwilk@gmail.com>
2018-10-09 10:13:42 -0400

    install express-session

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-var db = require("./models");
+const db = require("./models");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
