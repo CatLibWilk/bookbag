f66611c764fe945981d71e53cf83598aef51ab5d
Andy Wilk <andrewdwilk@gmail.com>
2018-10-24 16:30:56 -0400

    responsive stuff and fixed navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -20,34 +20,33 @@ class Navbar extends Component {
     };
     
     return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">BookBag U+</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item active">
-          <Link to={`/homepage/`}>
-            <div className="nav-link" href="#">Home <span className="sr-only">(current)</span></div>
-          </Link>
-        </li>
+     
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">BookBag U+</a>
+          <div>
+            <ul className="navbar-nav pr-2">
+              <li className="nav-item active d-inline">
+                <Link to={`/homepage/`}>
+                  <div className="nav-link" href="#">Home <span className="sr-only">(current)</span></div>
+                </Link>
+              </li>
+            
+              <li className="nav-item d-inline">
+              <Link to={"/"}>
+                <div className="nav-link" href="#">Signin</div>
+              </Link>
+              </li>
       
-        <li className="nav-item mr-auto">
-        <Link to={"/"}>
-          <div className="nav-link" href="#">Signin</div>
-        </Link>
-        </li>
-
-        <li className="nav-item mr-auto">
-          <a className="nav-link" href="#" onClick={this.handleLogout}>Signout</a>
-        </li>
-        
-      </ul>
-    </div>
-  </nav>
+              <li className="nav-item d-inline">
+                <a className="nav-link" href="#" onClick={this.handleLogout}>Signout</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
   )}
 }
 
 
 export default withRouter(Navbar);
+
+
